0a8270d230e9c61fca14b5dfb517021000d3e7ec
uncaffe <conillatte@gmail.com>
2024-09-08 21:54:28 +0200

    end of chapter03

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,23 @@ from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import SearchVector, \
+                                           SearchQuery, SearchRank, \
+                                           TrigramSimilarity
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    # Paging 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
     try:
@@ -22,8 +32,8 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     
     return render(request, 'blog/post/list.html',
-                           {'posts': posts})
-
+                           {'posts': posts,
+                            'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
@@ -36,11 +46,19 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active=True)
     # Form for users to comment
     form = CommentForm()
+    
+    # List of posts with related tags
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags','-publish')[:4]
+    
     return render(request, 'blog/post/detail.html',
                            {'post': post,
                             'comments': comments,
-                            'form': form})
-
+                            'form': form,
+                            'similar_posts': similar_posts})
 
 class PostListView(ListView):
     """
@@ -50,7 +68,6 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
-
 
 def post_share(request, post_id):
     # Get post by id
@@ -97,3 +114,22 @@ def post_comment(request, post_id):
                            {'post':post,
                             'form': form,
                             'comment': comment})
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+            
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
--- mysite/settings.py
+++ mysite/settings.py
@@ -30,6 +30,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -41,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -79,8 +84,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'piotr',
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD')
     }
 }
 
